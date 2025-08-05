e14079968763c13f7426b1164215e70038bcd62e
Nagesh-Nadimatla <nagesh3244@gmail.com>
2025-08-05 15:59:31 +0530

    changes in snapshot and order_items_uniq model file

--- oms_dbt_project/models/order_items_uniq.sql
+++ oms_dbt_project/models/order_items_uniq.sql
@@ -1,6 +1,9 @@
+SELECT 
+ORDERITEMID, ORDERID, PRODUCTID, QUANTITY, UNITPRICE, UPDATED_AT 
+FROM (
 {{ deduplicate(
     relation=source('landing', 'orderitm'),
     partition_by='orderid',
     order_by="updated_at desc",
    )
-}}
+}} )
--- oms_dbt_project/snapshots/customers_history.sql
+++ oms_dbt_project/snapshots/customers_history.sql
@@ -1,13 +1,19 @@
-{% snapshot customer_history %}
-
+{% snapshot customers_history %}
 {{
     config(
       target_schema='l3_consumption',
       unique_key='CUSTOMERID',
-      strategy='timestamp',
-      updated_at='updated_at',
+      strategy = 'check',
+      check_cols = ['city','STATE'],
+      invalidate_hard_deletes = True 
     )
 }}
+
+
+{# comment to use other strategy with timestamp column
+    strategy='timestamp',
+    updated_at='updated_at'
+#}
 
 
 SELECT * FROM {{ source('landing', 'cust') }}
